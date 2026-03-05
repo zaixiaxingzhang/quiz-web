1b91c0b2a8ab787e7497afe3c4a6dc3df1c4a85d
zaixiaxingzhang <161102392+zaixiaxingzhang@users.noreply.github.com>
2026-03-05 21:33:06 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'shuatibao-v2.3';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
